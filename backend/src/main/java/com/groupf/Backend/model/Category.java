92c58de74fdfca0eec412999208a62d9729f3f3c
Mardini8 <christian.mardini@tuta.com>
2025-04-18 11:07:10 +0200

    backend category constuctor

--- backend/src/main/java/com/groupf/Backend/model/Category.java
+++ backend/src/main/java/com/groupf/Backend/model/Category.java
@@ -1,11 +1,15 @@
 package com.groupf.Backend.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "categories")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
